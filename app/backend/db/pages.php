59334501806078907a831df6794204e48a03c948
Xarksass <disalvo.infographiste@gmail.com>
2020-04-16 14:01:26 +0200

    Fix page child list info (backend)

--- app/backend/db/pages.php
+++ app/backend/db/pages.php
@@ -107,7 +107,7 @@ class backend_db_pages
 						}
 					}
 
-					$sql = "SELECT p.id_pages, c.name_pages, c.content_pages, c.seo_title_pages, c.seo_desc_pages, p.menu_pages, p.date_register
+					$sql = "SELECT p.id_pages, c.name_pages, c.resume_pages, c.content_pages, c.seo_title_pages, c.seo_desc_pages, p.menu_pages, p.date_register, p.img_pages
 							FROM mc_cms_page AS p
 							JOIN mc_cms_page_content AS c USING ( id_pages )
 							JOIN mc_lang AS lang ON ( c.id_lang = lang.id_lang )
